bc573f22e33b6e8f8d28d91042e10f8833feb7a5
Salim Khalfouni <salimkhalfouni23@gmail.com>
2021-11-29 10:16:40 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import UserList from './component/UserList';
+
+function App() {
+  return (
+    <div>
+    <div class="sp-container">
+	<div class="sp-content">
+		<h2 class="frame-1">LIST</h2>
+		<h2 class="frame-2">OF</h2>
+		<h2 class="frame-3">USERS</h2>
+    <h2 class="frame-4">BYSkFlame</h2>
+	
+		
+	</div>
+</div>
+    <UserList/>
+    </div>
+  );
+}
+
+export default App;
